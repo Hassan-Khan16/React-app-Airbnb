fb5ab2c0e945829b26c9fbbf51f4bdf316249d26
Hassan Khan <hassankpisces@gmail.com>
2024-05-04 02:17:04 +0500

    Navbar and Hero

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import logo from "../images/airbnb.png"
+function Navbar() {
+    return (
+        <nav>
+            <img src={logo} />
+        </nav>
+    )
+}
+
+export default Navbar
