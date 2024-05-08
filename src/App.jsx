194c182491393d65e2008580230451a7c9f9c2b1
Hassan Khan <hassankpisces@gmail.com>
2024-05-08 22:33:14 +0500

    Props added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Cardrow from './components/Cardrow'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Hero />
-      <Cardrow />
+      {/* <Hero /> */}
+      <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      />
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,22 @@
 import React from "react"
-import cardImage from "../images/katie-zaferes.png"
 import statusImage from "../images/soldout.png"
 import starImage from "../images/star.png"
 
-function Card() {
+function Card(props) {
+    console.log(props)
     return (
-        <div className="card">
-            <img src={cardImage} className="card-image" />
+        < div className="card" >
+            <img src={`/src/images/${props.img}`} className="card-image" />
             <img src={statusImage} className="card-status" />
             <div className="card-details">
                 <img src={starImage} height={14} />
-                <span>5.0</span>
-                <span className="gray">(6) - </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) - </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <h2 className="card-details">Life lessons with Katie Zaferes</h2>
-            <p className="card-details"><b>From $136 </b>/ person</p>
-        </div>
+            <h2 className="card-details">{props.title}</h2>
+            <p className="card-details"><b>From ${props.price} </b>/ person</p>
+        </div >
     )
 }
 
