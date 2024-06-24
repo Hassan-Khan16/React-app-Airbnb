9e839ee03c9c2fb4c98f115c2921124e4f7fadf4
Hassan Khan <hassankpisces@gmail.com>
2024-06-25 03:41:48 +0500

    Responsive finished v1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,11 @@ function Hero() {
     return (
         <div className="hero">
             <img src={photogrid} className="hero-image" />
-            <h1 className="hero-header">Online Experiences</h1>
-            <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
-            </p>
+            <div className="hero-content">
+                <h1 className="hero-header">Online Experiences</h1>
+                <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                </p>
+            </div>
         </div>
     )
 }
